ffd26d0a680e32395e2bdb40a73fb7bd9b7b1788
Sarath <sarathskumbalath@gmail.com>
2020-07-19 15:36:47 +0530

    Added CandaInfoTableViewCell.swift, Added UITablview through code set constraints for the same and integrated datasource and delegate methods

---
+++ AboutCanada/Views/Cells/CandaInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CandaInfoTableViewCell.swift
+//  AboutCanada
+//
+//  Created by Sarath Sasi on 19/07/20.
+//  Copyright © 2020 Sarath Sasi. All rights reserved.
+//
+
+import UIKit
+
+class CandaInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
