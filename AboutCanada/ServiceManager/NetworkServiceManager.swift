fe64f4cb44687a34237c72f4ffb4900902d2ee00
Sarath <sarathskumbalath@gmail.com>
2020-07-19 22:15:55 +0530

    Removed unused UIImageview extension, and service manager methods

--- AboutCanada/ServiceManager/NetworkServiceManager.swift
+++ AboutCanada/ServiceManager/NetworkServiceManager.swift
@@ -26,7 +26,7 @@ final class NetworkServiceManager{
                 // make sure this JSON is in the format we expect
                 let utf8Data = String(decoding: data, as: UTF8.self).data(using: .utf8)
                 let data = try JSONDecoder().decode(BaseModel.self, from: utf8Data!)
-
+                
                 completion(.success(data))
             } catch {
                 completion(.failure(.jsonParsingError))
@@ -35,52 +35,14 @@ final class NetworkServiceManager{
         task.resume()
     }
     
-    ///
-    // MARK: - Private functions
-       private static func getData(url: URL,
-                                   completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-           URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
-       }
-       
-       // MARK: - Public function
-       
-       /// downloadImage function will download the thumbnail images
-       /// returns Result<Data> as completion handler
-       public static func downloadImage(url: URL,
-                                        completion: @escaping (Results<Data>) -> Void) {
-           NetworkServiceManager.getData(url: url) { data, response, error in
-               
-               if let error = error {
-                   completion(.failure(error))
-                   return
-               }
-               
-               guard let data = data, error == nil else {
-                   return
-               }
-               
-               DispatchQueue.main.async() {
-                   completion(.success(data))
-               }
-           }
-       }
-    
-
+    //URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    private struct Constants {
+        static let baseURL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/"
+        static let facts = "facts.json"
+    }
 }
-//URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
-private struct Constants {
-    static let baseURL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/"
-    static let facts = "facts.json"
-}
-
 enum NetworkError: Error {
-    case badURL
-    case jsonParsingError
-    case apiError
-}
-/// Result enum is a generic for any type of value
-/// with success and failure case
-public enum Results<T> {
-    case success(T)
-    case failure(Error)
-}
+     case badURL
+     case jsonParsingError
+     case apiError
+ }
