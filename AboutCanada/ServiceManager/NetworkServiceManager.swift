c60e628a726994803d74c43183ee92f389ca6439
Sarath <sarathskumbalath@gmail.com>
2020-07-19 15:03:11 +0530

    Implemented method to consume api and parse and assign to model

--- AboutCanada/ServiceManager/NetworkServiceManager.swift
+++ AboutCanada/ServiceManager/NetworkServiceManager.swift
@@ -11,6 +11,29 @@ import Foundation
 final class NetworkServiceManager{
     static let sharedInstance = NetworkServiceManager()
     private init() {}
+    
+    public func fetchCanadaFacts(withCompletion completion: @escaping (Result<BaseModel, NetworkError>) -> Void) {
+        guard let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts") else {
+            completion(.failure(.badURL))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(.apiError))
+                return
+            }
+            do {
+                // make sure this JSON is in the format we expect
+                let utf8Data = String(decoding: data, as: UTF8.self).data(using: .utf8)
+                let data = try JSONDecoder().decode(BaseModel.self, from: utf8Data!)
+
+                completion(.success(data))
+            } catch {
+                completion(.failure(.jsonParsingError))
+            }
+        }
+        task.resume()
+    }
 
 }
 //URL = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
--- AboutCanada/Views/ViewController/AboutCanadaListViewController.swift
+++ AboutCanada/Views/ViewController/AboutCanadaListViewController.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class AboutCanadaListViewController: UIViewController {
+    var factTitle: String?
+    var factArray: [Row]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
+        NetworkServiceManager.sharedInstance.fetchCanadaFacts { [weak self] result in
+            switch result {
+            case let .success(facts): print(facts)
+                // assing to variable
+            self?.factTitle = facts.title
+            self?.factArray = facts.rows
+            
+                DispatchQueue.main.async {
+                    
+                    self?.setUpNavigation()
+                }
+            case let .failure(error): self?.showAlert(errorType: .apiError)
+            }
+        }
     }
-    
+    func setUpNavigation() {
+        navigationItem.title = self.factTitle
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(displayP3Red: 0.2431372549, green: 0.7647058824, blue: 0.8392156863, alpha: 1)
+        self.navigationController?.navigationBar.isTranslucent = false
+    }
+    private func showAlert(errorType: NetworkError) {
+        //Add UIAlert after functionalty implementation
+        print("Alert!!!!!!:\(errorType)")
+    }
 
     /*
     // MARK: - Navigation
