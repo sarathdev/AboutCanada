31abf152d21f7b6095f2e2acc73615e35bf6081e
Sarath <sarathskumbalath@gmail.com>
2020-07-19 14:28:08 +0530

    Added the code to launch the initial view controller from App delegate and SceneDelegate, Added View Controller for listing facts

---
+++ AboutCanada/Views/ViewController/AboutCanadaListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AboutCanadaListViewController.swift
+//  AboutCanada
+//
+//  Created by Sarath Sasi on 19/07/20.
+//  Copyright © 2020 Sarath Sasi. All rights reserved.
+//
+
+import UIKit
+
+class AboutCanadaListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
